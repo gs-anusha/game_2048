51b2895e4e8bcdc1fee5764c4e0e4695e5a26840
kitkat2024 <98793066+kitkat2024@users.noreply.github.com>
2024-05-01 13:55:38 +0530

    Update game.py

--- game.py
+++ game.py
@@ -8,107 +8,11 @@ from pyqt_interface import Ui_MainWindow
 # temp_board = np.array([[4, 64, 2, 4], [32, 2, 1024, 1024], [64, 32, 8, 4], [2, 8, 4, 2]])
 
 
-class TerminalUI:
-    def display_t(self, board):
-        for row in board:
-            current = "|"
-            for element in row:
-                if element == 0:
-                    current += "    |"
-                else:
-                    current += str(element) + " " * (4 - len(str(element))) + "|"
-            print(current)
-
-
-class QtUI(QMainWindow, Ui_MainWindow):
+class Game2048:
     def __init__(self):
-        super().__init__()
         self.board_size = 4
-        self.setupUi(self)
-        self.setWindowTitle("2048 Game")
-
-        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
-
-        self.moves = QButtonGroup()
-
-    def display_qt(self, board):
-
-        self.game_board.clear()
-
-        self.game_board.setRowCount(self.board_size)
-        self.game_board.setColumnCount(self.board_size)
-
-        self.game_board.horizontalHeader().setVisible(False)
-        self.game_board.verticalHeader().setVisible(False)
-
-        for i in range(self.board_size):
-            for j in range(self.board_size):
-                value = board[i][j]
-                item = QTableWidgetItem(str(value))
-                if value != 0:
-                    item.setBackground(QtGui.QColor(255, 200, 0))
-                else:
-                    item.setBackground(QtGui.QColor(230, 220, 210))
-                item.setTextAlignment(QtCore.Qt.AlignCenter)
-                self.game_board.setItem(i, j, item)
-
-    def show_message(self, message):
-        msg = QMessageBox()
-        msg.setWindowTitle("2048 Game")
-        msg.setText(message)
-        msg.exec_()
-
-    def clear_selection(self):
-        self.moves.setExclusive(False)
-        for button in self.moves.buttons():
-            button.setChecked(False)
-        self.moves.setExclusive(True)
-
-    def set_square_cells(self):
-        for i in range(self.board_size):
-            self.game_board.setRowHeight(i, self.game_board.height() // self.board_size)
-            self.game_board.setColumnWidth(i, self.game_board.width() // self.board_size)
-
-        self.game_board.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.game_board.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-
-
-class Game2048(TerminalUI, QtUI):
-    def __init__(self):
-        super().__init__()
-        self.init_ui()
         self.current_board = self.initial_board()
         self.game_over = False
-        self.app = QApplication(sys.argv)
-        self.ui = None
-        self.select_ui()
-
-        self.show()
-
-    def init_ui(self):
-
-        self.moves.addButton(self.rb_left)
-        self.moves.addButton(self.rb_right)
-        self.moves.addButton(self.rb_up)
-        self.moves.addButton(self.rb_down)
-
-        self.rb_left.clicked.connect(self.start_game_qt)
-        self.rb_right.clicked.connect(self.start_game_qt)
-        self.rb_up.clicked.connect(self.start_game_qt)
-        self.rb_down.clicked.connect(self.start_game_qt)
-
-        self.restart.clicked.connect(self.restart_game)
-
-    def select_ui(self):
-        self.ui = input("Choose your interface. Type terminal or qt\n")
-        if self.ui == "terminal":
-            self.start_game_terminal()
-        elif self.ui == "qt":
-            self.start_game_qt()
-        else:
-            if self.ui is not None:
-                print("Try again")
-            self.select_ui()
 
     def initial_board(self):
         board = np.zeros((4, 4), dtype=int)
@@ -213,26 +117,41 @@ class Game2048(TerminalUI, QtUI):
     def handle_merge_board_down(self, board):
         return self.merge_board_down(board)
 
-    def start_game_terminal(self):
+
+class TerminalUI:
+    def __init__(self, game):
+        self.game = game
+
+    def display_t(self, board):
+        for row in board:
+            current = "|"
+            for element in row:
+                if element == 0:
+                    current += "    |"
+                else:
+                    current += str(element) + " " * (4 - len(str(element))) + "|"
+            print(current)
+
+    def start_game(self):
         info = "\nTo move left type L \nTo move right type R \nTo move up type U \nTo move down type D"
         print(info)
-        if self.game_over:
+        if self.game.game_over:
             print("GAME OVER")
         else:
-            self.display_t(self.current_board)
+            self.display_t(self.game.current_board)
             instruction = input("make your move\n").upper()
-            temp = self.current_board.copy()
+            temp = self.game.current_board.copy()
             if instruction == "L":
-                temp = self.merge_board_left(temp)
+                temp = self.game.merge_board_left(temp)
             elif instruction == "R":
-                temp = self.merge_board_right(temp)
+                temp = self.game.merge_board_right(temp)
             elif instruction == "U":
-                temp = self.merge_board_up(temp)
+                temp = self.game.merge_board_up(temp)
             elif instruction == "D":
-                temp = self.merge_board_down(temp)
+                temp = self.game.merge_board_down(temp)
             else:
                 print("Try again")
-                self.start_game_terminal()
+                self.start_game()
 
             largest = np.max(temp)
             if largest == 2048:
@@ -240,26 +159,95 @@ class Game2048(TerminalUI, QtUI):
                 print("You Win")
 
             else:
-                if not self.compare(self.current_board, temp):
-                    temp = self.add_num(temp)
+                if not self.game.compare(self.game.current_board, temp):
+                    temp = self.game.add_num(temp)
                 else:
-                    if self.zeroes_on_board(temp):
+                    if self.game.zeroes_on_board(temp):
                         print("Invalid move try again")
                     else:
-                        if self.possible_merges(temp):
+                        if self.game.possible_merges(temp):
                             print("Invalid move try again")
                         else:
-                            self.game_over = True
+                            self.game.game_over = True
 
-                self.current_board = temp
-                self.start_game_terminal()
+                self.game.current_board = temp
+                self.start_game()
 
-    def start_game_qt(self):
-        self.show()
-        self.display_qt(self.current_board)
+
+class QtUI(QMainWindow, Ui_MainWindow):
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.setupUi(self)
+        self.setWindowTitle("2048 Game")
+
+        self.setWindowFlags(self.windowFlags() | QtCore.Qt.WindowStaysOnTopHint)
+
+        self.moves = QButtonGroup()
+        self.init_ui()
+
+        self.start_game()
+
+    def init_ui(self):
+
+        self.moves.addButton(self.rb_left)
+        self.moves.addButton(self.rb_right)
+        self.moves.addButton(self.rb_up)
+        self.moves.addButton(self.rb_down)
+
+        self.rb_left.clicked.connect(self.start_game)
+        self.rb_right.clicked.connect(self.start_game)
+        self.rb_up.clicked.connect(self.start_game)
+        self.rb_down.clicked.connect(self.start_game)
+
+        self.restart.clicked.connect(self.restart_game)
+
+    def display_qt(self, board):
+
+        self.game_board.clear()
+
+        self.game_board.setRowCount(self.game.board_size)
+        self.game_board.setColumnCount(self.game.board_size)
+
+        self.game_board.horizontalHeader().setVisible(False)
+        self.game_board.verticalHeader().setVisible(False)
+
+        for i in range(self.game.board_size):
+            for j in range(self.game.board_size):
+                value = board[i][j]
+                item = QTableWidgetItem(str(value))
+                if value != 0:
+                    item.setBackground(QtGui.QColor(255, 200, 0))
+                else:
+                    item.setBackground(QtGui.QColor(230, 220, 210))
+                item.setTextAlignment(QtCore.Qt.AlignCenter)
+                self.game_board.setItem(i, j, item)
+
+    def show_message(self, message):
+        msg = QMessageBox()
+        msg.setWindowTitle("2048 Game")
+        msg.setText(message)
+        msg.exec_()
+
+    def clear_selection(self):
+        self.moves.setExclusive(False)
+        for button in self.moves.buttons():
+            button.setChecked(False)
+        self.moves.setExclusive(True)
+
+    def set_square_cells(self):
+        for i in range(game.board_size):
+            self.game_board.setRowHeight(i, self.game_board.height() // self.game.board_size)
+            self.game_board.setColumnWidth(i, self.game_board.width() // self.game.board_size)
+
+        self.game_board.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+        self.game_board.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
+
+    def start_game(self):
+        self.display_qt(self.game.current_board)
         self.set_square_cells()
 
-        if self.game_over:
+        if self.game.game_over:
             self.show_message("Game Over")
         else:
             selected_button = self.moves.checkedButton()
@@ -268,15 +256,15 @@ class Game2048(TerminalUI, QtUI):
                 return
 
             direction_map = {
-                self.rb_left: self.handle_merge_board_left,
-                self.rb_right: self.handle_merge_board_right,
-                self.rb_up: self.handle_merge_board_up,
-                self.rb_down: self.handle_merge_board_down
+                self.rb_left: self.game.handle_merge_board_left,
+                self.rb_right: self.game.handle_merge_board_right,
+                self.rb_up: self.game.handle_merge_board_up,
+                self.rb_down: self.game.handle_merge_board_down
             }
 
             handle_function = direction_map[selected_button]
 
-            temp = self.current_board.copy()
+            temp = self.game.current_board.copy()
             temp = handle_function(temp)
 
             largest = np.max(temp)
@@ -285,31 +273,37 @@ class Game2048(TerminalUI, QtUI):
                 self.show_message("You Win")
                 self.restart_game()
             else:
-                if not self.compare(self.current_board, temp):
-                    temp = self.add_num(temp)
+                if not self.game.compare(self.game.current_board, temp):
+                    temp = self.game.add_num(temp)
                 else:
-                    if self.zeroes_on_board(temp):
+                    if self.game.zeroes_on_board(temp):
                         self.show_message("Invalid move try again")
                     else:
-                        if self.possible_merges(temp):
+                        if self.game.possible_merges(temp):
                             self.show_message("Invalid move try again")
                         else:
-                            self.game_over = True
+                            self.game.game_over = True
 
-                self.current_board = temp
-                self.display_qt(self.current_board)
+                self.game.current_board = temp
+                self.display_qt(self.game.current_board)
                 self.clear_selection()
 
     def restart_game(self):
-        self.current_board = self.initial_board()
-        self.game_over = False
+        self.game.current_board = self.game.initial_board()
+        self.game.game_over = False
         self.clear_selection()
-        if self.ui == "qt":
-            self.start_game_qt()
+        self.start_game()
 
 
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    game = Game2048()
-    game.show()
-    sys.exit(app.exec_())
+    game = Game2048()  # Create an instance of Game2048
+    ui = input("Choose your interface. Type terminal or qt\n").lower()
+    if ui == "terminal":
+        terminal_ui = TerminalUI(game)  # Pass the Game2048 instance to TerminalUI
+        terminal_ui.start_game()
+    elif ui == "qt":
+        app = QApplication(sys.argv)
+        qt_ui = QtUI(game)  # Pass the Game2048 instance to QtUI
+        qt_ui.show()
+        sys.exit(app.exec_())
+
